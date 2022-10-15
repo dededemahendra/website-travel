eab413cf102f2760c2843a2cbe3cc631fb2ad0ef
Dede Mahendra <75364408+dededemahendra@users.noreply.github.com>
2022-10-15 15:15:07 +0800

    Menambahkan paket travel

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,8 @@
 const menuToggle = document.querySelector("#input");
 const navbar = document.querySelector(" nav ul");
+const nav = document.querySelector(" nav ");
 
 menuToggle.addEventListener("click", function () {
   navbar.classList.toggle("slide");
+  nav.classList.toggle("nav");
 });
